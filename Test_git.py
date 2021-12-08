ba6567caea42f1b32ad17273360424b64287e25d
KoshkinGeorge <goshak495@gmail.com>
2021-12-08 15:56:58 +0200

    Refreshed merge sorting

--- Test_git.py
+++ Test_git.py
@@ -1,26 +1,29 @@
 import random
+from math import floor
 
 
-def quick_sort(arr):
-    equal = []
-    less = []
-    higher = []
-    if arr == []:
-        return []
-    chosen = random.choice(arr)
-    for i in arr:
-        if i < chosen:
-            less.append(i)
-        elif i == chosen:
-            equal.append(i)
+def merge(arr1, arr2):
+    ans = []
+    while arr1 != [] and arr2 != []:
+        if arr1[0] > arr2[0]:
+            ans.append(arr2[0])
+            del arr2[0]
         else:
-            higher.append(i)
-    return quick_sort(less) + equal + quick_sort(higher)
+            ans.append(arr1[0])
+            del arr1[0]
+    return ans + arr1 + arr2
+
+
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    else:
+        return merge(merge_sort(arr[:len(arr) // 2]), merge_sort(arr[len(arr) // 2:]))
 
 
 array = []
 for i in range(1024):
     array.append(int(random.random() * 10000))
 print(array)
-array = quick_sort(array)
+array = merge_sort(array)
 print(array)
