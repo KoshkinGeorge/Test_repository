817f96d7f2c5082713e6e997f1f0840c408d160a
KoshkinGeorge <goshak495@gmail.com>
2021-12-10 23:46:35 +0200

    Solved some exercises using graphs

--- Test_git.py
+++ Test_git.py
@@ -1,25 +1,38 @@
 def lowest_pos(d):
     m = float('inf')
     ans = None
-    for k, v in d:
-        if v < m:
+    for k, v in d.items():
+        if v < m and k not in checked:
             ans = k
             m = v
     return ans
 
 
+checked = set()
 graph = dict()
-graph['n'] = {'a': 6, 'b': 2}
-graph['a'] = {'k': 1}
-graph['b'] = {'a': 3, 'k': 5}
-graph['k'] = {}
+graph['a'] = {'b': 1, 'c': 5}
+graph['b'] = {'c': 6, 'f': 2, 'g': 10, 'e': 3}
+graph['c'] = {'d': 7}
+graph['d'] = {'f': 1}
+graph['e'] = {'g': 8, 'h': 4}
+graph['f'] = {'g': 5}
+graph['h'] = {'g': 3}
 costs = dict()
-costs['n'] = 0
-costs['a'] = 6
-costs['b'] = 2
-costs['k'] = float('inf')
-parents = dict()
+costs['a'] = 0
+costs['b'] = 1
+costs['c'] = 5
+costs['d'] = float('inf')
+costs['e'] = float('inf')
+costs['f'] = float('inf')
+costs['h'] = float('inf')
+costs['g'] = float('inf')
 cur = lowest_pos(costs)
 while cur is not None:
-    negh = graph[cur]
-    for i in negh:
+    for k, v in graph[cur].items():
+        if costs[k] > costs[cur] + graph[cur][k]:
+            costs[k] = costs[cur] + graph[cur][k]
+    checked.add(cur)
+    cur = lowest_pos(costs)
+    if cur == 'g':
+        break
+print('Easiest way: ', costs['g'])
