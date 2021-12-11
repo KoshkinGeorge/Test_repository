ccc9b9c23b1a5002736d4124bb8c890ad7961d69
KoshkinGeorge <goshak495@gmail.com>
2021-12-11 15:29:49 +0200

    Refreshed OOP

--- Test_git.py
+++ Test_git.py
@@ -1,38 +1,25 @@
-def lowest_pos(d):
-    m = float('inf')
-    ans = None
-    for k, v in d.items():
-        if v < m and k not in checked:
-            ans = k
-            m = v
-    return ans
+class MyClass:
+    name = 'bob'
+
+    def hi(self, spec=None):
+        if spec == None:
+            print(f'My name is {self.name}')
+        else:
+            print(f'My name is {spec.name}')
+
+    def change_name(self):
+        self.name = str(input("Enter new name: "))
+
+class f:
+    pass
 
 
-checked = set()
-graph = dict()
-graph['a'] = {'b': 1, 'c': 5}
-graph['b'] = {'c': 6, 'f': 2, 'g': 10, 'e': 3}
-graph['c'] = {'d': 7}
-graph['d'] = {'f': 1}
-graph['e'] = {'g': 8, 'h': 4}
-graph['f'] = {'g': 5}
-graph['h'] = {'g': 3}
-costs = dict()
-costs['a'] = 0
-costs['b'] = 1
-costs['c'] = 5
-costs['d'] = float('inf')
-costs['e'] = float('inf')
-costs['f'] = float('inf')
-costs['h'] = float('inf')
-costs['g'] = float('inf')
-cur = lowest_pos(costs)
-while cur is not None:
-    for k, v in graph[cur].items():
-        if costs[k] > costs[cur] + graph[cur][k]:
-            costs[k] = costs[cur] + graph[cur][k]
-    checked.add(cur)
-    cur = lowest_pos(costs)
-    if cur == 'g':
-        break
-print('Easiest way: ', costs['g'])
+class Kryit(MyClass):
+    name = 'Press f'
+
+
+base = MyClass()
+pios = Kryit()
+base.hi()
+pios.hi()
+print(super(Kryit))
